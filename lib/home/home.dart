259c4c29487084f1ddfc176882dba0aae25b7930
leejia0324 <hx.un0707@gmail.com>
2025-12-14 23:37:30 +0900

    홈페이지 게시물 피드 구현

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -8,18 +8,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // 스크롤시 사라지게 구현하기
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            scrolledUnderElevation: 0,
-            backgroundColor: Colors.white,
-            leadingWidth: 300,
-            leading: Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 10, bottom: 10),
-                  child: Text(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              scrolledUnderElevation: 0,
+              backgroundColor: Colors.white,
+
+              titleSpacing: 10,
+              title: Row(
+                children: [
+                  Text(
                     'Instagram',
                     style: TextStyle(
                       fontFamily: 'Norican',
@@ -27,66 +28,164 @@ class HomePage extends StatelessWidget {
                       fontSize: 30,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 5, bottom: 5),
-                  child: Icon(Symbols.keyboard_arrow_down),
-                ),
-              ],
-            ),
-
-            actions: [
-              Padding(
-                padding: EdgeInsets.only(right: 15),
-                child: Icon(
-                  Symbols.app_settings_alt,
-                  weight: 600,
-                  color: Colors.black,
-                  size: 30,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 15),
-                child: Icon(
-                  Symbols.favorite,
-                  weight: 600,
-                  size: 30,
-                  color: Colors.black,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Icon(
-                  Symbols.send,
-                  weight: 600,
-                  size: 30,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-            // backgroundColor: Colors.green,
-          ),
-
-          SliverToBoxAdapter(
-            child: Container(
-              height: 130,
-              // color: Colors.black,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: _storyItem('내 스토리'),
-                        ),
-                        _storyItem('sxe1xo'),
-                        _storyItem('ss0_xa'),
-                        _storyItem('5.219_'),
-                        _storyItem('gracexilver'),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5, bottom: 5),
+                    child: Icon(Symbols.keyboard_arrow_down),
+                  ),
                 ],
               ),
+
+              actions: [
+                Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Icon(
+                    Symbols.app_settings_alt,
+                    weight: 600,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 15),
+                  child: Icon(
+                    Symbols.favorite_rounded,
+                    weight: 600,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Icon(
+                    Symbols.send,
+                    weight: 600,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+              // backgroundColor: Colors.green,
             ),
-          ),
-        ], // Slivers
+
+            SliverToBoxAdapter(
+              child: Container(
+                height: 130,
+                // color: Colors.black,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Stack(
+                        children: [
+                          _storyItem('내 스토리'),
+                          Positioned(
+                            right: 17,
+                            bottom: 27,
+                            child: Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: 20,
+                            bottom: 30,
+                            child: Icon(Icons.add_circle),
+                          ),
+                        ],
+                      ),
+                    ),
+                    _storyItem('sxe1xo'),
+                    _storyItem('ss0_xa'),
+                    _storyItem('5.219_'),
+                    _storyItem('gracexilver'),
+                  ],
+                ),
+              ),
+            ),
+
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Column(
+                  children: [
+                    Container(
+                      width: 390,
+                      height: 50,
+                      // color: Colors.red,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Container(
+                              width: 35,
+                              height: 35,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    'assets/images/ProfileImage.jpg',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 15, top: 5),
+                                child: Text(
+                                  'limse0rin',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Text(
+                                  '회원님을 위한 추천',
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 200),
+                            child: Icon(Icons.more_horiz),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(width: 390, height: 390, color: Colors.black),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.favorite_border_rounded, size: 30,),
+                        ),
+                        Text('10'),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Symbols.mode_comment, opticalSize: 40, size: 27)
+                        ),
+                        Text('5'),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Symbols.send, size: 30, opticalSize: 40,)
+                        )
+                      ],
+                    ),
+                  ],
+                ), // Column - 사진 피드 1개
+              ]),
+            ),
+          ], // Slivers
+        ),
       ), // CustomScrollView
     ); // Scaffold
   }
